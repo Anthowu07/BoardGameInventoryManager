c1d8813e738b6382fda52b590e507f2f263a5375
Anthowu07 <anthony07wu@gmail.com>
2024-08-20 16:51:23 -0400

    adjust cors

--- backend/src/main/java/com/skillstorm/project1/config/CorsConfig.java
+++ backend/src/main/java/com/skillstorm/project1/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Allow all paths
-                        .allowedOrigins("http://localhost:5173") // Allow specific origin
+                        .allowedOrigins("http://boardgame-inventory-management.s3-website-us-east-1.amazonaws.com") // Allow specific origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Allow credentials
