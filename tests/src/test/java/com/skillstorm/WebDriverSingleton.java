b866a3e2bc6fd07edd8ef36c4ca3b678469d0a71
Anthowu07 <anthony07wu@gmail.com>
2024-08-28 15:49:27 -0400

    debug

--- tests/src/test/java/com/skillstorm/WebDriverSingleton.java
+++ tests/src/test/java/com/skillstorm/WebDriverSingleton.java
@@ -4,13 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverSingleton {
-
+    
+    
     private static WebDriver driver;
 
     private WebDriverSingleton() {
     }
 
     public static WebDriver getDriver() {
+        System.setProperty("webdriver.chrome.driver", "\\usr\\bin\\chromedriver.exe");
         if (driver == null) {
             driver = new ChromeDriver();
         }
